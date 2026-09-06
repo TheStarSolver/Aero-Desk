100743a64f3125605000ec22378ab2fe0c4b1e64
TheStarSolver <andreydamascenod77@outlook.com>
2026-09-06 16:22:53 -0300

    Configure remote database connection

--- server/server.js
+++ server/server.js
@@ -10,10 +10,21 @@ require("dotenv").config({ path: "./.env" });
 const crypto = require("crypto");
 
 const db = mysql.createConnection({
+
     host: process.env.DB_HOST,
+
     user: process.env.DB_USER,
+
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+
+    database: process.env.DB_NAME,
+
+    port: process.env.DB_PORT,
+
+    ssl: {
+        rejectUnauthorized: true
+    }
+
 });
 
 db.connect((err) => {
